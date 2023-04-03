c33bc776f41f8a432166abeb8afc7a7488138cae
burgerking59 <oliverbk4@gmail.com>
2023-04-03 17:35:57 +0100

    new commit

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -10,7 +10,8 @@
             <h1 class="form-label">Are you sure you want to delete {{ $project -> title }}?</h1>
             <div class="row">
                 <input type="submit" class="form-control btn btn-primary col m-2" value="Delete">
-                <a href="{{ route('projects.show', $project -> pid) }}" class="btn btn-primary col m-2">Cancel</a>
+                <a href="{{ route('projects.show', $project -> pid) }}"  class="btn btn-primary col m-2">Back to Project</a><br>
+                <a href="{{ route('home') }}"  class="btn btn-primary col m-2">Back to Dashboard</a>
             </div>
         </form>
         
